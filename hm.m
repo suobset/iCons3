feef425179aa4cae7a175bae09630bd231ba9352
Anvitha <anvitha2k20@gmail.com>
2023-02-21 17:57:25 -0500

    now can look at any floor's heatmap data

--- hm.m
+++ hm.m
@@ -1,15 +1,23 @@
-% reads floor1.csv and displays the heatmap as a floor-view for one floor.
+% Anvitha Ramachandran
+% iCons 389H - Spring 2023
+% Library Heatmaps By Floor
+% Created: 2/20/23
 
-data = readtable("floor1.csv")
-
+% reads in all the data for the library by floor
+floors = readtable("librarydata.csv");
+% formatting csv for ideal format of data to be in heatmap accurately.
+data = readtable("floor1.csv");
+% select a floor to view
+fl = input('What floor do you want to view? ');
+temp = floors(fl,:);
+temp = removevars(temp, "Floor");
+% writes the data for the perceived temp in the formatting style
+data.PerceivedTemp = table2array(temp).';
 h = heatmap(data, 'SideOfLederle', 'SideOfPond', "ColorVariable", "PerceivedTemp");
-
-
-% reordering the categories
+% reordered the columns so the data appears sensibly in the heatmap.
 h.SourceTable.SideOfLederle = categorical(h.SourceTable.SideOfLederle);
 neworder = ["Lederle", "middle", "Lederle'"];
 h.SourceTable.SideOfLederle = reordercats(h.SourceTable.SideOfLederle,neworder);
-
 h.SourceTable.SideOfPond = categorical(h.SourceTable.SideOfPond);
 neworder = ["Pond", "middle", "Pond'"];
 h.SourceTable.SideOfPond = reordercats(h.SourceTable.SideOfPond,neworder);
