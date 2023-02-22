85e953ec8b52aa7d89a8ad5e8722b5aa0fb8ef73
Anvitha <anvitha2k20@gmail.com>
2023-02-22 00:16:08 -0500

    slider for all the floors.

--- hm.m
+++ hm.m
@@ -7,13 +7,12 @@
 floors = readtable("librarydata.csv");
 % formatting csv for ideal format of data to be in heatmap accurately.
 data = readtable("floor1.csv");
-% select a floor to view
-fl = input('What floor do you want to view? ');
-temp = floors(fl,:);
+UIFigure = uifigure('Position',[0 0 600 500]);
+temp = floors(1,:);
 temp = removevars(temp, "Floor");
 % writes the data for the perceived temp in the formatting style
 data.PerceivedTemp = table2array(temp).';
-h = heatmap(data, 'SideOfLederle', 'SideOfPond', "ColorVariable", "PerceivedTemp");
+h = heatmap(UIFigure, data, 'SideOfLederle', 'SideOfPond', "ColorVariable","PerceivedTemp");
 % reordered the columns so the data appears sensibly in the heatmap.
 h.SourceTable.SideOfLederle = categorical(h.SourceTable.SideOfLederle);
 neworder = ["Lederle", "middle", "Lederle'"];
@@ -21,3 +20,23 @@ h.SourceTable.SideOfLederle = reordercats(h.SourceTable.SideOfLederle,neworder);
 h.SourceTable.SideOfPond = categorical(h.SourceTable.SideOfPond);
 neworder = ["Pond", "middle", "Pond'"];
 h.SourceTable.SideOfPond = reordercats(h.SourceTable.SideOfPond,neworder);
+UISlider = uislider(UIFigure, 'Position',[20, 500, 200, 3]);
+UISlider.Limits = [1, 21];
+UISlider.ValueChangedFcn = {@UISliderValueChangedFcn, data, floors};
+function UISliderValueChangedFcn(Source, ~, data, floors)
+    sliderValue = round(Source.Value); % We need to round the decimal value we get from UISlider.Value to index into img
+    Source.Value = sliderValue; % Change slider to rounded value to show it on UIFigure
+    UIFigure = Source.Parent;
+    temp = floors(sliderValue,:);
+    temp = removevars(temp, "Floor");
+    % writes the data for the perceived temp in the formatting style
+    data.PerceivedTemp = table2array(temp).';
+    h = heatmap(UIFigure, data, 'SideOfLederle', 'SideOfPond', "ColorVariable", "PerceivedTemp");
+    % reordered the columns so the data appears sensibly in the heatmap.
+    h.SourceTable.SideOfLederle = categorical(h.SourceTable.SideOfLederle);
+    neworder = ["Lederle", "middle", "Lederle'"];
+    h.SourceTable.SideOfLederle = reordercats(h.SourceTable.SideOfLederle,neworder);
+    h.SourceTable.SideOfPond = categorical(h.SourceTable.SideOfPond);
+    neworder = ["Pond", "middle", "Pond'"];
+    h.SourceTable.SideOfPond = reordercats(h.SourceTable.SideOfPond,neworder);
+end
